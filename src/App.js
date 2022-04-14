bc6ce51385f0090f60926e5b53e3dfe42d14ae2d
taoufik-elmassira <83514059+timitar77@users.noreply.github.com>
2022-04-14 17:21:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function App() {
+  const [term, setTerm] = useState("javascript");
+  const [debounceSearch, setDebounceSearch] = useState(term);
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setDebounceSearch(term);
+    }, 1000);
+    return () => clearTimeout(timeOut);
+  }, [term]);
+
+  useEffect(() => {
+    if (debounceSearch) {
+      const search = async () => {
+        const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: debounceSearch
+          }
+        });
+        setResult(respond.data.query?.search);
+      };
+
+      search();
+    }
+  }, [debounceSearch]);
+
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const respond = await axios.get('https://en.wikipedia.org/w/api.php', {
+  //       params: {
+  //         action: 'query',
+  //         list: 'search',
+  //         origin: '*',
+  //         format: 'json',
+  //         srsearch: term,
+  //       },
+  //     });
+  //     setResult(respond.data.query.search);
+  //   };
+
+  //   if (!result.length) {
+  //     if (term) {
+  //       search();
+  //     }
+  //   } else {
+  //     const debounceSearch = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 1200);
+
+  //     return () => {
+  //       clearTimeout(debounceSearch);
+  //     };
+  //   }
+  // }, [term, result.length]);
+
+  //init render
+  //useEffect -> check length -> search() -> update resualt
+  //re-render
+  //useEffect ->check length -> search() -> update resualt
+  //re-render
+
+  const fetchResult = result.map((el) => {
+    return (
+      <tr key={el.pageid}>
+        <td>1</td>
+        <td>{el.title}</td>
+        <td>
+          <span dangerouslySetInnerHTML={{ __html: el.snippet }} />
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="my-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Search Input
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleFormControlInput1"
+              onChange={(e) => setTerm(e.target.value)}
+              value={term}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Desc</th>
+              </tr>
+            </thead>
+            <tbody>{fetchResult}</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
